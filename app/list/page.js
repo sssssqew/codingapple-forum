9deced368d9502dc7fd325310ff55fdbb801af60
sssssqew <sunrise.in.my.dream@gmail.com>
2023-11-11 11:59:55 +0900

    페이지 캐슁과 다이나믹 렌더링 적용함

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,8 @@
 import connectDB from "@/util/database"
 import ListItem from "./ListItem"
 
+export const dynamic = 'force-dynamic' // 다이나믹 렌더링
+export const revalidate = 60 // 60초동안 페이지를 캐슁함 (서버자원 절약)
 
 // DB 조회하는 컴포넌트는 서버 컴포넌트로 작성하기
 export default async function List(){
